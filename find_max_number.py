3d19e65c1a5c9c9cbae4ad017595b3d05aaca8dd
Arjun <jadhavarjun53@gmail.com>
2021-08-12 15:36:11 +0530

    [DilipRathod] FloatMaxNumberFromThreeNumber

---
+++ find_max_number.py
@@ -0,0 +1,32 @@
+class FindMaximumNumber:
+    num1 = 0
+    num2 = 0
+    num3 = 0
+
+    # parameterized constructor
+    def __init__(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+    def find_max_int_num(self):
+
+        if (self.num1 > self.num2) and (self.num1 > self.num3):
+            return self.num1
+        elif (self.num2 > self.num1) and (self.num2 > self.num3):
+            return self.num2
+        elif (self.num3 > self.num1) and (self.num1 > self.num2):
+            return self.num3
+        else:
+            print("All number are same")
+
+    def find_max_float_num(self):
+
+        if (self.num1 > self.num2) and (self.num1 > self.num3):
+            return self.num1
+        elif (self.num2 > self.num1) and (self.num2 > self.num3):
+            return self.num2
+        elif (self.num3 > self.num1) and (self.num1 > self.num2):
+            return self.num3
+        else:
+            print("All number are same")
